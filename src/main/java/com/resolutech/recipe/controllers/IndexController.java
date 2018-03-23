01c193886e7d4e0c6ca8ebff4bd744b6e40b7f98
Jean-Francois Larouche <jean-francois.larouche@dti.ulaval.ca>
2018-03-23 15:27:24 -0400

    Section 99

--- src/main/java/com/resolutech/recipe/controllers/IndexController.java
+++ src/main/java/com/resolutech/recipe/controllers/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping({"", "/", "index"})
     String getIndexPage() {
+        System.out.println("Index requested");
         return "index";
     }
 }
