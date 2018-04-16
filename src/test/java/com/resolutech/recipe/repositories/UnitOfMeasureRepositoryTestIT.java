a932a6726fdbc0e144943843cdc96fd456892fc9
Jean-Francois Larouche <jean-francois.larouche@dti.ulaval.ca>
2018-04-16 14:21:11 -0400

    Section 127

---
+++ src/test/java/com/resolutech/recipe/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -0,0 +1,46 @@
+package com.resolutech.recipe.repositories;
+
+import com.resolutech.recipe.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Important Integration test with full spring context
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryTestIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    @DirtiesContext
+    public void findByDescription() {
+        Optional<UnitOfMeasure> uomOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOfMeasure.get().getDescription());
+
+    }
+
+    @Test
+    public void findByDescriptionCup() {
+        Optional<UnitOfMeasure> uomOfMeasure = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", uomOfMeasure.get().getDescription());
+
+    }}
