7dcde85d6cb78b77027e43fad6bf410785ecfcd2
Jean-Francois Larouche <jean-francois.larouche@dti.ulaval.ca>
2018-04-25 15:37:14 -0400

    Section 146

---
+++ src/main/java/com/resolutech/recipe/repositories/IngredientRepository.java
@@ -0,0 +1,9 @@
+package com.resolutech.recipe.repositories;
+
+import com.resolutech.recipe.domain.Ingredient;
+import com.resolutech.recipe.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IngredientRepository extends CrudRepository<Ingredient, Long> {
+
+}
