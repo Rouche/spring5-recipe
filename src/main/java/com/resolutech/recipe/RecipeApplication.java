614defe42754bb4c3afc9819445187f7d2ded28e
Jean-Francois Larouche <jean-francois.larouche@dti.ulaval.ca>
2018-05-09 09:38:19 -0400

    Section 211 CodeCov JaCoCo

--- src/main/java/com/resolutech/recipe/RecipeApplication.java
+++ src/main/java/com/resolutech/recipe/RecipeApplication.java
@@ -2,11 +2,18 @@ package com.resolutech.recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RecipeApplication {
+public class RecipeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RecipeApplication.class);
 	}
 }
