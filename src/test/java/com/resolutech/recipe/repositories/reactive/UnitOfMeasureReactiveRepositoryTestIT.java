32d15b7dffcb2fb64fa1d6db14b6f866613d549a
Jean-Francois Larouche <jean-francois.larouche@dti.ulaval.ca>
2018-05-17 09:58:02 -0400

    Section 234 Reactive

--- src/test/java/com/resolutech/recipe/repositories/reactive/UnitOfMeasureReactiveRepositoryTestIT.java
+++ src/test/java/com/resolutech/recipe/repositories/reactive/UnitOfMeasureReactiveRepositoryTestIT.java
@@ -1,20 +1,12 @@
 package com.resolutech.recipe.repositories.reactive;
 
-import com.resolutech.recipe.bootstrap.RecipeBootstrap;
 import com.resolutech.recipe.domain.UnitOfMeasure;
-import com.resolutech.recipe.repositories.CategoryRepository;
-import com.resolutech.recipe.repositories.RecipeRepository;
-import com.resolutech.recipe.repositories.UnitOfMeasureRepository;
-import com.resolutech.recipe.repositories.reactive.UnitOfMeasureReactiveRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
