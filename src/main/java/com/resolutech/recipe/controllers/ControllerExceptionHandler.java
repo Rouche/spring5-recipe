4001bd15b830603000e9d0240faff0acc9cd729b
Jean-Francois Larouche <rouche@gmail.com>
2018-05-25 11:12:52 -0400

    Section 257 ControlerAdvice reloaded

--- src/main/java/com/resolutech/recipe/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/resolutech/recipe/controllers/ControllerExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.resolutech.recipe.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 /**
  *
@@ -10,10 +15,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleNumberFormat(Exception exception) {
-//        return ErrorUtils.getErrorView(exception, "400 Bad request");
-//    }
+    // @Important web exchange
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleNumberFormat(Exception exception, Model model) {
 
+        log.error("Handling " + exception.getClass().getName());
+        log.error(exception.getMessage());
+
+        return ErrorUtils.getErrorView(model, exception, "400 Bad request");
+    }
 }
