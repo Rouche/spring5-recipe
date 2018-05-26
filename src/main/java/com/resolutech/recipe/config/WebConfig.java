6d6bfe5b432557b9fa4b8df46c59756c606fab11
Jean-Francois Larouche <rouche@gmail.com>
2018-05-25 22:14:21 -0400

    Section 259 ignore failed tests

--- src/main/java/com/resolutech/recipe/config/WebConfig.java
+++ src/main/java/com/resolutech/recipe/config/WebConfig.java
@@ -14,6 +14,7 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 @Configuration
 public class WebConfig {
 
+    // @Important config with a route, WebFlux only.
     @Bean
     RouterFunction<?> routes(RecipeService recipeService) {
         return RouterFunctions.route(GET("/api/recipes"),
